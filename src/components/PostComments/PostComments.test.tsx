73eeb82b7acd63e4923a0cf085d9b374353df3c1
ariel <you@example.com>
2025-06-08 18:23:30 -0300

    adicionando o teste de 2 comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Muito legal!'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-campo-comentar'))
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Irado!'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-campo-comentar'))
+
+        expect(screen.getAllByTestId('numero-comentarios')).toHaveLength(2);
     });
 });
